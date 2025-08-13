b2903065891911c6737196fddec0efb4454af754
Denis Listiadi <denislistiadi24@gmail.com>
2025-08-13 10:01:46 +0700

    feat: array associative

--- array.php
+++ array.php
@@ -3,3 +3,11 @@
 // indeks
 $fruits = ["apple", "orange", "banana"];
 echo$fruits[1];
+
+// associative
+$person = [
+    "name" => "denis listiadi",
+    "age" => 25,
+    "city" => "pemalang"
+];
+echo $person["name"];
