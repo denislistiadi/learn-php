d31a7dcb9e76d02b9d1a3d1b03d4fbbcbf36ee97
Denis Listiadi <denislistiadi24@gmail.com>
2025-08-13 10:04:32 +0700

    feat: array multidimensional

--- array.php
+++ array.php
@@ -2,12 +2,29 @@
 
 // indeks
 $fruits = ["apple", "orange", "banana"];
-echo$fruits[1];
+echo $fruits[1];
 
 // associative
 $person = [
-    "name" => "denis listiadi",
-    "age" => 25,
-    "city" => "pemalang"
+  "name" => "denis listiadi",
+  "age" => 25,
+  "city" => "pemalang"
 ];
 echo $person["name"];
+
+// multidimensional
+$people = [
+  [
+    "name",
+    "denis listiadi"
+  ],
+  [
+    "age",
+    25
+  ],
+  [
+    "city",
+    "pemalang"
+  ]
+];
+echo $people[0][1]; 
